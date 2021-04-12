033d25f9b2377401f99b912577cf39e2f62920d5
CryoMyst <CryoMyst@hotmail.com>
2021-04-12 15:09:47 +1000

    Documentation changes only

--- DIFactoryBuilder.SourceGenerator/Extensions/IParameterSymbolExtensions.cs
+++ DIFactoryBuilder.SourceGenerator/Extensions/IParameterSymbolExtensions.cs
@@ -1,15 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Microsoft.CodeAnalysis;
+﻿// ***********************************************************************
+// Assembly         : DIFactoryBuilder.SourceGenerator
+// Author           : CryoM
+// Created          : 04-12-2021
+//
+// Last Modified By : CryoM
+// Last Modified On : 04-12-2021
+// ***********************************************************************
+// <copyright file="IParameterSymbolExtensions.cs" company="DIFactoryBuilder.SourceGenerator">
+//     Copyright (c) . All rights reserved.
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
 
 namespace DIFactoryBuilder.SourceGenerator.Extensions
 {
+    using Microsoft.CodeAnalysis;
+
+    /// <summary>
+    /// Class IParameterSymbolExtensions.
+    /// </summary>
     public static class IParameterSymbolExtensions
     {
+        /// <summary>
+        /// Determines whether the specified attribute symbol has attribute.
+        /// </summary>
+        /// <param name="typeSymbol">The type symbol.</param>
+        /// <param name="attributeSymbol">The attribute symbol.</param>
+        /// <returns><c>true</c> if the specified attribute symbol has attribute; otherwise, <c>false</c>.</returns>
         public static bool HasAttribute(this IParameterSymbol typeSymbol, INamedTypeSymbol attributeSymbol)
         {
             foreach (var attribute in typeSymbol.GetAttributes())
--- DIFactoryBuilder/Attributes/RequiresFactoryAttribute.cs
+++ DIFactoryBuilder/Attributes/RequiresFactoryAttribute.cs
@@ -1,11 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// ***********************************************************************
+// Assembly         : DIFactoryBuilder
+// Author           : CryoM
+// Created          : 04-09-2021
+//
+// Last Modified By : CryoM
+// Last Modified On : 04-09-2021
+// ***********************************************************************
+// <copyright file="RequiresFactoryAttribute.cs" company="DIFactoryBuilder">
+//     Copyright (c) . All rights reserved.
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
 
 namespace DIFactoryBuilder.Attributes
 {
+    using System;
+
+    /// <summary>
+    /// Class RequiresFactoryAttribute.
+    /// Implements the <see cref="System.Attribute" />
+    /// </summary>
+    /// <seealso cref="System.Attribute" />
     [AttributeUsage(AttributeTargets.Class)]
     public class RequiresFactoryAttribute : Attribute
     {
